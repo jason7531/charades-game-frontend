b86ade77880cdd60de834209da84e545cd37e045
jason7531 <54910136+jason7531@users.noreply.github.com>
2021-08-27 00:33:33 +0530

    cards added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import "./App.css";
+import React from "react";
 
-function App() {
-  return <div className="App"></div>;
-}
+import Chat from "./components/Chat/Chat";
+import Join from "./components/Join/Join";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Router>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
+    </Router>
+  );
+};
 
 export default App;
